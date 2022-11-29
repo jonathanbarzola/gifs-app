fb5a3df90077d21b9972ff306dc6e430c17eaffc
jonathanbarzola <jonabl2102@gmail.com>
2022-11-29 13:17:14 -0500

    GIFS APP

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent {
+
+}
---
+++ src/app/gifs/resultados/resultados.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resultados',
+  templateUrl: './resultados.component.html',
+  styles: [
+  ]
+})
+export class ResultadosComponent {
+
+}
